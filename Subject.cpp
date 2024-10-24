eaeac18f9ffa59c75f7c92784a9af4bfaee7f17c
Franco <franco.v.d.westhuizen@gmail.com>
2024-10-24 16:38:02 +0200

    Implemented the Subject class for the Observer

---
+++ Subject.cpp
@@ -0,0 +1,16 @@
+#include "Subject.h"
+#include <iostream>
+#include <algorithm>
+
+void Subject::attach(Citizen *observer){
+
+    observerList.push_back(observer);
+
+}
+
+void Subject::detach(Citizen *observer){
+
+    observerList.erase(std::remove(observerList.begin(), observerList.end(), observer), observerList.end());
+
+
+}
--- Subject.h
+++ Subject.h
@@ -1,16 +1,20 @@
 #ifndef SUBJECT_H
 #define SUBJECT_H
 
+#include <iostream>
+#include <vector>
+
 #include "Citizen.h"
 
-class Subject
-{
-private:
-    Citizen* observerList;
-public:
-    void attach(Citizen* observer);
-    void detach(Citizen* observer);
-    void notify();
+class Subject{
+
+    private:
+        std::vector<Citizen*> observerList;
+    public:
+        void attach(Citizen* observer);
+        void detach(Citizen* observer);
+        virtual void notify(std::string, float) = 0;
+
 };
 
 

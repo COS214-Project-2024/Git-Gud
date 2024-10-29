a661d49be479d14b9942f954926f68a9b1731d1c
SaltyNomad <swanepoellukas5@gmail.com>
2024-10-29 13:55:13 +0200

    Revert "Merge branch 'feature/GameEnvironment' into development"
    This reverts commit 1df8953fbb9c2b3568c7eee5c9c742cbbd0eef98, reversing
    changes made to 34c252a80eedce67335479f4baeda88c0e6d5885.

--- GameEnvironment.h
+++ GameEnvironment.h
@@ -1,50 +1,29 @@
 #ifndef GAMEENVIRONMENT_H
 #define GAMEENVIRONMENT_H
 
-#include <iostream>
-#include <fstream>
+#include "Utility.h"
+#include "Citizen.h"
+#include "Building.h"
+#include "UtilityIterator.h"
+#include "BuildingIterator.h"
 #include <vector>
-#include "UtilityGridNode.h"
-/* #include "ConcreteIterators.h"
-#include "RadialIterator.h"
-#include "LinearIterator.h" */
-using namespace std;
-//placeholder
-class Utility;
+
 class Building;
-//forward declarations for actual utility
-//class Iterator;
-class Citizen;
 
-class GameEnvironment {
+class GameEnvironment
+{
 private:
+    Utility*** utilityGrid;
     Building*** buildingGrid;
-    UtilGridNode*** utilityGrid;
-    vector<Citizen*> citizens;
-    friend class Iterator;
-    friend class Building;
-
     int rows, cols;
-    int startingPoint[2];//Starting point of the map shal be offramp at the centre. The centre point changes with the size of the map. Intially it is set to 25,25
-    void resizeGrid(int newRows, int newCols, string direction);
-    bool adjToRoad(int x, int y);
 public:
-    GameEnvironment();
-    ~GameEnvironment();
-
-    bool add(Building* building, int x, int y);
-    bool add(Utility* utility, int x, int y);
-    bool remove(int x, int y, string type="unspecified");
-    void printTerminal();
-    void printToFile(string filename="map.txt");
-    void createRoad(int x, int y);
-    
-    /* RadialBuildingIterator* createRadBuildIt(int x, int y, int radius);
-    RadialUtilityIterator* createRadUtilIt(int x, int y, int radius);
-    UtilityIterator* createUtilIt();
-    BuildingIterator* createBuildIt(); */
-
-    
+    void addUtility(Utility* utility, int x, int y);
+    void addBuilding(Building* building, int x, int y);
+    void remove();
+    void PrintToTerminal();
+    void PrintToFIle();
+    UtilityIterator* createUtilityIterator();
+    BuildingIterator* createBuildingIterator();
 
 };
 
--- Iterator.h
+++ Iterator.h
@@ -1,25 +1,14 @@
 #ifndef ITERATOR_H
 #define ITERATOR_H
 
-#include <string>
-#include "GameEnvironment.h"
-
-using namespace std;
-
-template <typename T>
-class Iterator {
-    protected:
-        T*** array;
-        GameEnvironment* game;
-
-    public:
-        Iterator();
-        ~Iterator();
-
-        virtual bool hasNext()=0;
-        virtual void next()=0;
-        virtual T* current() const=0;
-    
+class Iterator
+{
+private:
+ 
+public:
+    virtual void next() = 0;
+    virtual void hasNext() = 0;
+    virtual bool current() = 0;
 };
 
 #endif // ITERATOR_H

f5f9aa7c7b09d9e39ca1abaa8a09c72fc88858d7
WJvdwalt <104898337+WJvdwalt@users.noreply.github.com>
2024-10-31 11:59:13 +0200

    BuildingFactory Unit testing added

---
+++ BuildingFactoryTest.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include "CommercialBuildingFactory.h"
+#include "BuildingWithCoffeeShopFactory.h"
+
+TEST(CommercialBuildingFactoryTest, construct){
+    BuildingFactory* f=new CommercialBuildingFactory();
+    CommercialBuilding* b=f->constructBuilding();
+    
+    EXPECT_EQ(b->getBusinessType(), BusinessType::GENERAL);
+}
+
+TEST(CoffeeShopFactoryTest, construct){
+    BuildingFactory* f=new BuildingWithCoffeeShopFactory();
+    Building* old=new CommercialBuilding(50,100,3,BusinessType::GENERAL);
+    Building* b=f->upgradeBuilding(old);
+    CommercialBuilding* c=static_cast<CommercialBuilding*>(b);
+
+    EXPECT_EQ(c->getBusinessType(), BusinessType::GENERAL);
+}

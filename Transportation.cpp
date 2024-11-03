ab4dbd2fc19448ce4f4ed039b894264c9fae4da1
MAutumn11 <markautumn11@gmail.com>
2024-11-03 11:21:30 +0200

    Chnages to transportation to make it a subject and call functions

--- Transportation.cpp
+++ Transportation.cpp
@@ -1,7 +1,31 @@
 #include "Transportation.h"
+#include "TrafficBuilding.h"
+#include "TrafficBuildingFactory.h"
 #include "Subject.h"
 #include <iostream>
+#include <stdexcept>
+#include <iostream>
 
+Transportation::Transportation(){
+
+    for(Citizen* citizen : allCitizens){
+
+        attach(citizen);
+
+    }
+
+    if(allCitizens.size() > 0){
+
+            ChangeData changeData = {"ProvideService", 8.0f};
+
+            this->notify(changeData);   
+
+            std::cout << "Transportation has been made available in the area" << std::endl;
+
+    }
+
+    
+}
 void Transportation::updateCapacity(TransportType t, int add)
 {
     switch(t)
@@ -24,33 +48,104 @@ void Transportation::updateCapacity(TransportType t, int add)
         }
 }
 
-TrafficFlow Transportation::calculateTrafficFlow()
+void Transportation::calculateTrafficFlow()
 {
-    /*
-    int remain = getNumCitizens() % getNumStations();
+    
+    int remain = getNumCitizens() - getTotalTrafficCapacity();
 
-    switch(remain)
+    if(remain > 50)
     {
-        case remain == 0:
-            currentFlow = HIGH;
-        break;
+        currentFlow = LOW;
 
-        case remain < 1:
-            currentFlow = MODERATE;
-        break;
+    }else if(remain <= 50 && remain > 0)
+    {
+        currentFlow = MODERATE;
 
-        case remain > 1:
-            currentFlow = LOW;
+    }else if(remain <= 0)
+    {
+        currentFlow = HIGH;
     }
-    */
+  
 }
 
-void Transportation::callObserver(){
 
-    ChangeData changeData = {"ProvideService", 8.0f};
-
-    this->notify(changeData);   
-
-    std::cout << "Transportation has been made available in the area" << std::endl;
-
+int Transportation::getTotalTrafficCapacity(){
+    
+    return busCapacity + taxiCapacity + trainCapacity + airportCapacity;
 }
+
+void Transportation::addTrafficBuilding(TransportType type, int cost) {
+    auto building = TrafficBuildingFactory::createTrafficBuilding(type, cost);
+    trafficBuildings[type].push_back(std::unique_ptr<TrafficBuilding>(building));
+    switch(type)
+        {
+            case TAXI:
+                numTaxis++;
+                break;
+            case BUS:
+                numBuses++;
+                break;
+            case TRAIN:
+                numTrains++;
+                break;
+            case AIRPORT:
+                numAirports++;
+                break;
+            default:
+                break;
+        
+        }
+}
+
+void Transportation::initializeTrafficFlow() {
+    // Initialize traffic flow management
+    std::cout << "Traffic flow is being managed across the following buildings:" << std::endl;
+    for (const auto& typePair : trafficBuildings) {
+        std::cout << " - " << getNumStations(typePair.first) << " buildings of type " << getTransportTypeString(typePair.first) << std::endl;
+    }
+}
+
+
+std::string Transportation::getTransportTypeString(TransportType t)  {
+        switch(t)
+        {
+            case TAXI:
+                return "Taxi";
+                break;
+            case BUS:
+                return "Bus";
+                break;
+            case TRAIN:
+                return "Train";
+                break;
+            case AIRPORT:
+                return "Airport";
+                break;
+            default:
+                return "No type";
+        
+        }
+    }
+
+    int Transportation::getNumStations(TransportType t)  {
+        switch(t)
+        {
+            case TAXI:
+                return numTaxis;
+                break;
+            case BUS:
+                return numBuses;
+                break;
+            case TRAIN:
+                return numTrains;
+                break;
+            case AIRPORT:
+                return numAirports;
+                break;
+            default:
+                return 0;
+        
+        }
+    }
+
+    
--- Transportation.h
+++ Transportation.h
@@ -1,12 +1,23 @@
 #ifndef TRANSPORTATION_H
 #define TRANSPORTATION_H
 
+#include <vector>
+#include <memory>
+#include <unordered_map>
 #include "PublicService.h"
 #include "TrafficBuilding.h"
 
 enum TrafficFlow{LOW, MODERATE, HIGH};
-class Transportation : public PublicService {
+class Transportation : public Subject {
 private:
+
+    std::unordered_map<TransportType, std::vector<std::unique_ptr<TrafficBuilding>>> trafficBuildings;
+
+    int numTaxis;
+    int numTrains;
+    int numAirports;
+    int numBuses;
+
     int busCapacity;
     int taxiCapacity;
     int trainCapacity;
@@ -14,14 +25,28 @@ private:
 
     TrafficFlow currentFlow;
 public:
+
+    Transportation();
     //Increase amount of each transport type
     //Used to calculate the traffic flow for all the citizens using that transport type
     void updateCapacity(TransportType t, int add);
 
     //Calculates Traffic flow based on the total number of citizens in city and the capacity of the stations
-    TrafficFlow calculateTrafficFlow();
+    void calculateTrafficFlow();
 
-    void callObserver() override;
+    //Used to get overall transport capacity in the city
+    int getTotalTrafficCapacity();
+
+    void addTrafficBuilding(TransportType type, int capacity);
+
+    // Initializes traffic flow if validation passes for each type
+    void initializeTrafficFlow();
+
+    //Get number of stations for a type (Use to check if there are minimum 2)
+    int getNumStations(TransportType t);
+
+    //Get name of transport in string
+    std::string getTransportTypeString(TransportType t);
 };
 
 #endif  // TRANSPORTATION_H

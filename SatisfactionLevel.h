e5fe5c3178e55bdaed888de71cab4feb445b3ddd
MAutumn11 <markautumn11@gmail.com>
2024-11-03 17:45:15 +0200

    Doxygen comments for Mark

--- SatisfactionLevel.h
+++ SatisfactionLevel.h
@@ -13,11 +13,29 @@ class SatisfactionLevel{
     public:
         float satisfactionRating;
 
+        
         virtual ~SatisfactionLevel(){};
+
+        /**
+        * @brief Method used to enter the next state
+        **/
         virtual void enterState() = 0;
+
+        /**
+        * @brief Method used to exit the current state
+        **/
         virtual void exitState() = 0;
 
+        /**
+        * @brief Method used to retrieve the current satisfaction level
+        * @return std::string
+        **/
         virtual std::string getCurrentSatisfactionLevel() = 0;
+
+        /**
+        * @brief Method used to retrieve the tax return of the current state
+        * @return float
+        **/
         virtual float getTaxReturn() = 0;
 };
 
--- Transportation.h
+++ Transportation.h
@@ -26,28 +26,68 @@ private:
     TrafficFlow currentFlow;
 public:
 
+    /**
+        * @brief Constructor of Transportation
+        * @return
+    **/
     Transportation();
+
+    /**
+        * @brief Update the capacity of a transport type
+        * @param t
+        * @param add
+        * @return
+    **/
     //Increase amount of each transport type
     //Used to calculate the traffic flow for all the citizens using that transport type
     void updateCapacity(TransportType t, int add);
 
-    //Calculates Traffic flow based on the total number of citizens in city and the capacity of the stations
+    /**
+        * @brief Calculate traffic flow based on the total number of citizens in city and the capacity of the stations
+        * @return
+    **/
     void calculateTrafficFlow();
 
+    /**
+        * @brief Get current trafficFlow string name
+        * @param t
+        * @return string
+    **/
     std::string getCurrentFlow(TrafficFlow t);
 
-    //Used to get overall transport capacity in the city
+    /**
+        * @brief Get total traffic capacity for the city
+        * @return int
+    **/
     int getTotalTrafficCapacity();
 
+    /**
+        * @brief Add a traffic building to the city
+        * @param type
+        * @param capacity
+        * @return 
+    **/
     void addTrafficBuilding(TransportType type, int capacity);
 
-    // Initializes traffic flow if validation passes for each type
+    /**
+        * @brief Initializes traffic flow if validation passes for each type
+        * @return 
+    **/
     void initializeTrafficFlow();
 
-    //Get number of stations for a type (Use to check if there are minimum 2)
+    /**
+        * @brief Get number of stations for a type (Use to check if there are minimum 2)
+        * @param t
+        * @return int
+    **/
     int getNumStations(TransportType t);
 
-    //Get name of transport in string
+    /**
+        * @brief Get name of transport in string
+        * @param t
+        * @return string
+    **/
+    //
     std::string getTransportTypeString(TransportType t);
 };
 
